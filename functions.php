40ef8bee1cbd315568612df90845d73404fc94ab
Phil Boothman <philboothman90@gmail.com>
2021-07-29 15:54:03 +0100

    Updated functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,20 @@
 <?php
 
+// Adding CSS and JS files
+
 function pb_setup() {
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'pb_setup');
+
+// Adding Theme Support
+
+function pb_init() {
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
+    add_theme_support('html5',
+        array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
+}
+
+add_action('after_setup_theme', 'pb_init');
