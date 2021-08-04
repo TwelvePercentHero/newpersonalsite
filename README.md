14884d004a9f3cd2ded75aa14946611576940a33
Phil Boothman <philboothman90@gmail.com>
2021-08-04 13:28:08 +0100

    Added header.php skeleton

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Personal Site Redesign
 
-This is a complete redesign of my personal website at [philboothman.com](https://philboothman.com) coinciding with a shift in focus from filmmaking to coding and website design and development.
+This is a complete redesign of my personal website at [philboothman.com](https://philboothman.com) coinciding with a shift in focus from filmmaking to website design and development.
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phil Boothman | In Development</title>
+</head>
+<body>
+
+<nav>
+    <a href="<?php echo site_url('')?>">
+        <h1>Phil Boothman</h1>
+    </a>
+    <ul>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">About</a></li>
+    </ul>
+</nav>
